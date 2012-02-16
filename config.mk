df549fd1d9cd85edf85a877de11a7f40585e01c4
Mathieu J. Poirier <mathieu.poirier@linaro.org>
2012-02-16 23:25:26 +0000

    snowball: android: moved from optimizing for size to optimizing for speed
    Optimizing for size with gcc 4.6.x prevents uboot from reaching the cmd
    line.  Removing this optimization solves the problem.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- config.mk
+++ config.mk
@@ -137,7 +137,7 @@ ARFLAGS = crv
 endif
 RELFLAGS= $(PLATFORM_RELFLAGS)
 DBGFLAGS= -g # -DDEBUG
-OPTFLAGS= -Os #-fomit-frame-pointer
+OPTFLAGS= -O2 #-fomit-frame-pointer
 ifndef LDSCRIPT
 #LDSCRIPT := $(TOPDIR)/board/$(BOARDDIR)/u-boot.lds.debug
 ifeq ($(CONFIG_NAND_U_BOOT),y)
